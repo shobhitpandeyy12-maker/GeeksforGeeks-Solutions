f1e94254cad0cf76ccc7dcb36ea4fe75160e4d25
Shobhit! <shobhitpandeyy12@gmail.com>
2026-07-17 23:03:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    int countWays(int n) {
+        // code here
+        if(n<=1)
+        return 1;
+        
+        
+        int s1=1;
+        int s2=1;
+        for(int i=2; i<=n; i++){
+            
+            int current=s1+s2;
+            s1=s2;
+            s2=current;
+            
+        }
+        
+        return s2;
+        
+    
+        
+            
+        
+        
+    }
+};
